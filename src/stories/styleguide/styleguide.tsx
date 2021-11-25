0d0b60ac173c637ce7bef835009745f2887db3c8
Anke <pfeiffer.anke@outlook.com>
2021-11-25 10:46:28 +0100

    :lipstick: Add colors to styleguide

--- src/stories/styleguide/styleguide.tsx
+++ src/stories/styleguide/styleguide.tsx
@@ -3,7 +3,7 @@ import styles from "./styleguide.module.css";
 export default function Styleguide() {
   return (
     <div>
-      <div className={styles.brickColors}>
+      <section className={styles.brickColors}>
         brick colors
         <div className={styles.redLight}>red-light</div>
         <div className={styles.greenLight}>green-light</div>
@@ -11,7 +11,19 @@ export default function Styleguide() {
         <div className={styles.redDark}>red-dark</div>
         <div className={styles.greenDark}>green-dark</div>
         <div className={styles.blueDark}>blue-dark</div>
-      </div>
+      </section>
+      <section className={styles.backgroundShades}>
+        shades of background
+        <div className={styles.backgroundLight}>lighter</div>
+        <div className={styles.background}>background</div>
+        <div className={styles.backgroundDark}>darker</div>
+      </section>
+      <section className={styles.backgroundShades}>
+        color theme minifig
+        <div className={styles.colorButton}>color-button</div>
+        <div className={styles.colorTextPrimary}>color-text-primary</div>
+        <div className={styles.colorTextSecondary}>color-text-secondary</div>
+      </section>
     </div>
   );
 }
