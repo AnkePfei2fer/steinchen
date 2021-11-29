1e4fa66fc5af3dc1f2c2386ebd90f5c52e6c5fcf
Anke <pfeiffer.anke@outlook.com>
2021-11-29 22:17:09 +0100

    :sparkles: Display message if search was unsucessful

--- src/app/components/Search/search.tsx
+++ src/app/components/Search/search.tsx
@@ -2,17 +2,18 @@ import { FormEvent, useState } from "react";
 
 import styles from "./search.module.css";
 
-type searchResultsProps = {
+type searchResultProps = {
   set_num: string;
   name: string;
   num_parts: number;
   set_img_url: string;
   theme_id: number;
+  detail: string;
 };
 
 export default function Search(): JSX.Element {
   const [query, setQuery] = useState<string | null>(null);
-  const [searchResults, setSearchResults] = useState<searchResultsProps | null>(
+  const [searchResult, setSearchResult] = useState<searchResultProps | null>(
     null
   );
 
@@ -22,38 +23,44 @@ export default function Search(): JSX.Element {
       `http://localhost:3001/api/sets/search_by_set_number/${query}`
     );
     console.log(response);
-    const results = await response.json();
-    setSearchResults(results);
-    console.log(searchResults);
+    const result = await response.json();
+    setSearchResult(result);
+    console.log(searchResult);
+    console.log(searchResult?.detail);
   };
 
   let content;
 
-  if (searchResults) {
+  if (!searchResult === null) {
     content = (
       <div className={styles.card}>
-        <span className={styles.text}>{searchResults.name}</span>
-        <img className={styles.image} src={searchResults.set_img_url} />
-        <p className={styles.theme}>Thema Nummer {searchResults.theme_id}</p>
-        <p className={styles.parts}>{searchResults.num_parts} Teile</p>
+        <span className={styles.text}>{searchResult?.name}</span>
+        <img className={styles.image} src={searchResult?.set_img_url} />
+        <p className={styles.theme}>Thema Nummer {searchResult?.theme_id}</p>
+        <p className={styles.parts}>{searchResult?.num_parts} Teile</p>
         <div className={styles.overlay}></div>
       </div>
-      // )}else if (query) {content =(
-      //   <span>Wir haben das Set leider nicht gefunden. Versuchs nochmal!</span>
+    );
+  } else {
+    content = (
+      <>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className={styles.textInput}
+            onChange={(event) => setQuery(event.target.value)}
+          ></input>
+          <button className={styles.button}>Suche</button>
+        </form>
+        {searchResult?.detail === "Not found." &&
+          (content = (
+            <span>
+              Wir haben Dein Set leider nicht gefunden. Versuchs nochmal!
+            </span>
+          ))}
+      </>
     );
   }
 
-  return (
-    <div className={styles.container}>
-      <form className={styles.searchForm} onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className={styles.textInput}
-          onChange={(event) => setQuery(event.target.value)}
-        ></input>
-        <button className={styles.button}>Suche</button>
-      </form>
-      {content}
-    </div>
-  );
+  return <div className={styles.container}>{content}</div>;
 }
