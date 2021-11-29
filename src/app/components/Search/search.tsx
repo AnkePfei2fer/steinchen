eddbbd3322a3f68e161d4b76922b6b79c8497909
Anke <pfeiffer.anke@outlook.com>
2021-11-29 21:22:40 +0100

    :sparkles: Display search results as cards

--- src/app/components/Search/search.tsx
+++ src/app/components/Search/search.tsx
@@ -2,8 +2,19 @@ import { FormEvent, useState } from "react";
 
 import styles from "./search.module.css";
 
-export default function Search() {
-const [query, setQuery] = useState<string | null>(null);
+type searchResultsProps = {
+  set_num: string;
+  name: string;
+  num_parts: number;
+  set_img_url: string;
+  theme_id: number;
+};
+
+export default function Search(): JSX.Element {
+  const [query, setQuery] = useState<string | null>(null);
+  const [searchResults, setSearchResults] = useState<searchResultsProps | null>(
+    null
+  );
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -12,13 +23,37 @@ const [query, setQuery] = useState<string | null>(null);
     );
     console.log(response);
     const results = await response.json();
-    console.log(results);
+    setSearchResults(results);
+    console.log(searchResults);
   };
 
+  let content;
+
+  if (searchResults) {
+    content = (
+      <div className={styles.card}>
+        <span className={styles.text}>{searchResults.name}</span>
+        <img className={styles.image} src={searchResults.set_img_url} />
+        <p className={styles.theme}>Thema Nummer {searchResults.theme_id}</p>
+        <p className={styles.parts}>{searchResults.num_parts} Teile</p>
+        <div className={styles.overlay}></div>
+      </div>
+      // )}else if (query) {content =(
+      //   <span>Wir haben das Set leider nicht gefunden. Versuchs nochmal!</span>
+    );
+  }
+
   return (
-    <form className={styles.searchForm} onSubmit={handleSubmit}>
-      <input type={"text"} className={styles.textInput} onChange={(event)=>setQuery(event.target.value)}></input>
-      <button className={styles.button}>Suche</button>
-    </form>
+    <div className={styles.container}>
+      <form className={styles.searchForm} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className={styles.textInput}
+          onChange={(event) => setQuery(event.target.value)}
+        ></input>
+        <button className={styles.button}>Suche</button>
+      </form>
+      {content}
+    </div>
   );
 }
