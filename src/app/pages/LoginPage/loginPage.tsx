6e8f83f0895a15450513688f4e21d0576eb4d178
Anke <pfeiffer.anke@outlook.com>
2021-12-01 15:11:30 +0100

    :lipstick: Style Login Page

--- src/app/pages/LoginPage/loginPage.tsx
+++ src/app/pages/LoginPage/loginPage.tsx
@@ -4,7 +4,17 @@ import Login from "../../components/Login/login";
 export default function LoginPage() {
   return (
     <div className={styles.container}>
-      <span className={styles.title}>STEINCHEN</span>
+      <div className={styles.title}>
+        <span className={styles.S}>S</span>
+        <span className={styles.T}>T</span>
+        <span className={styles.E}>E</span>
+        <span className={styles.I}>I</span>
+        <span className={styles.N}>N</span>
+        <span className={styles.C}>C</span>
+        <span className={styles.H}>H</span>
+        <span className={styles.E}>E</span>
+        <span className={styles.N}>N</span>
+      </div>
       <h3 className={styles.enterName}>Gib Deinen Namen ein</h3>
       <img
         className={styles.minifig}
