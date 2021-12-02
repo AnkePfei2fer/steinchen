24b3a87ee319f324ecbec1c2244050a0f5b56c50
Anke <pfeiffer.anke@outlook.com>
2021-12-02 16:20:54 +0100

    remove comments

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,14 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./globals.css";
 // import App from "./App";
 import LoginPage from "./pages/LoginPage/loginPage";
-// import WelcomePage from "./pages/WelcomePage/welcomePage";
 import Search from "./components/Search/search";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LoginPage />}>
-        {/* <Route path="/pages/WelcomePage/" element={<WelcomePage />} /> */}
         <Route path="/pages/search/" element={<Search />} />
       </Route>
     </Routes>
