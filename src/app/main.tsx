3408c9695da99e77725a40f7d2331365f8c6e637
Anke <pfeiffer.anke@outlook.com>
2021-12-02 14:27:27 +0100

    :heavy_plus_sign: Install react-router-dom
    prepare routes

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./globals.css";
-import App from "./App";
+// import App from "./App";
+import LoginPage from "./pages/LoginPage/loginPage";
+// import WelcomePage from "./pages/WelcomePage/welcomePage";
+import Search from "./components/Search/search";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />}>
+        {/* <Route path="/pages/WelcomePage/" element={<WelcomePage />} /> */}
+        <Route path="/pages/search/" element={<Search />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
