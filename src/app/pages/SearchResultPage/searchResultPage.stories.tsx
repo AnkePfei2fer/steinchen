a35dd151a369cf500d97f808af95b52098b00dcb
Anke <pfeiffer.anke@outlook.com>
2021-12-07 11:45:04 +0100

    :sparkles: Prepare search result page for storybook

---
+++ src/app/pages/SearchResultPage/searchResultPage.stories.tsx
@@ -0,0 +1,4 @@
+import SearchResultPage from "./searchResultPage";
+
+export default { component: SearchResultPage, title: "Pages/Search Result" };
+export const Default = () => <SearchResultPage />;
