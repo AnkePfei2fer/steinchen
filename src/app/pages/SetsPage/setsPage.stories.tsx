d27c46f21467fe0d2fdd092124f863146d7e7b4b
Anke <pfeiffer.anke@outlook.com>
2021-12-14 14:04:07 +0100

    :bug: Fix bug in stories pages

--- src/app/pages/SetsPage/setsPage.stories.tsx
+++ src/app/pages/SetsPage/setsPage.stories.tsx
@@ -1,4 +1,9 @@
 import SetsPage from "./setsPage";
+import { Set } from "../../types";
+
+type CollectionProps = { collection: Set[] };
 
 export default { component: SetsPage, title: "Pages/Sets" };
-export const Default = () => <SetsPage />;
+export const Default = ({ collection }: CollectionProps) => (
+  <SetsPage collection={collection} />
+);
