1e26349548a290c838997501e971345cbc4b66d2
Anke <pfeiffer.anke@outlook.com>
2021-12-15 13:53:35 +0100

    :bug: Add Props to stories from searchResult and details page

--- src/app/pages/DetailsPage/detailsPage.stories.tsx
+++ src/app/pages/DetailsPage/detailsPage.stories.tsx
@@ -1,9 +1,9 @@
 import DetailsPage from "./detailsPage";
 import { Set } from "../../types";
 
-type CollectionProps = { collection: Set[] };
+type CollectionProps = { collection: Set[]; onAddSet: () => void };
 
 export default { component: DetailsPage, title: "Pages/Details" };
-export const Default = ({ collection }: CollectionProps) => (
-  <DetailsPage collection={collection} />
+export const Default = ({ collection, onAddSet }: CollectionProps) => (
+  <DetailsPage collection={collection} onAddSet={onAddSet} />
 );
