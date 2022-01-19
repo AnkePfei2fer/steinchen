3210f525fcbdb0dcd7e8c829e45c64c8c9d4f6e4
Anke <pfeiffer.anke@outlook.com>
2022-01-19 12:06:51 +0100

    :bug: Fix type problems in stories

--- src/app/pages/BricksPage/bricksPage.stories.tsx
+++ src/app/pages/BricksPage/bricksPage.stories.tsx
@@ -1,7 +1,4 @@
 import BricksPage from "./bricksPage";
-import { CollectionProps } from "../../types";
 
 export default { component: BricksPage, title: "Pages/Bricks" };
-export const Default = ({ collection }: CollectionProps) => (
-  <BricksPage collection={collection} />
-);
+export const Default = () => <BricksPage />;
